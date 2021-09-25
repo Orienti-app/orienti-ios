58f99ef8333f2fed237b0e1d8cfa1ef6412d034e
Kryštof Kiss <krystof.kiss@backbone.sk>
2021-09-25 12:20:45 +0200

    initialize project

---
+++ Orienti/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Orienti
+//
+//  Created by Kryštof Kiss on 25/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("My sme orienti!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Orienti/OrientiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrientiApp.swift
+//  Orienti
+//
+//  Created by Kryštof Kiss on 25/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct OrientiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
