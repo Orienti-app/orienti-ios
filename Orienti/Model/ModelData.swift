749739c65369a7c9383f458144858b3fad29e7a8
Kryštof Kiss <krystof.kiss@backbone.sk>
2021-09-26 13:53:38 +0200

    add ModelData

---
+++ Orienti/Model/ModelData.swift
@@ -0,0 +1,76 @@
+//
+//  ModelData.swift
+//  Orienti
+//
+//  Created by Kryštof Kiss on 25/09/2021.
+//
+
+import Foundation
+
+//struct PersistentData: Codable {
+//    let user: User
+//}
+
+//final class ModelData: ObservableObject {
+    
+//    @Published var user: User
+    
+//    init() {
+//        self.user = User(name: "Krystof")
+//    }
+    
+//}
+
+
+// MARK: - Persisting ModelData
+//extension ModelData {
+//    private static var documentsFolder: URL {
+//        do {
+//            return try FileManager.default.url(for: .documentDirectory,
+//                                                  in: .userDomainMask,
+//                                                  appropriateFor: nil,
+//                                                  create: false)
+//        } catch {
+//            fatalError("Can't find documents directory.")
+//        }
+//    }
+//
+//    private static var fileURL: URL {
+//        return documentsFolder.appendingPathComponent("model.data")
+//    }
+//
+//    func save() {
+//        DispatchQueue.global(qos: .background).async { [weak self] in
+//            guard let scrums = self?.scrums else {
+//                fatalError("Self out of scope")
+//            }
+//
+//            guard let data = try? JSONEncoder().encode(scrums) else {
+//                fatalError("Error encoding data")
+//            }
+//
+//            do {
+//                let outfile = Self.fileURL
+//                try data.write(to: outfile)
+//            } catch {
+//                fatalError("Can't write to file")
+//            }
+//        }
+//    }
+//
+//    func load() {
+//        DispatchQueue.global(qos: .background).async { [weak self] in
+//            guard let data = try? Data(contentsOf: Self.fileURL) else {
+//                return
+//            }
+//
+//            guard let dailyScrums = try? JSONDecoder().decode([DailyScrum].self, from: data) else {
+//                fatalError("Can't decode saved model data.")
+//            }
+//
+//            DispatchQueue.main.async {
+//                self?.scrums = dailyScrums
+//            }
+//        }
+//    }
+//}
